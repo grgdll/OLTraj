6fa2abe8ade30c3b9efed8af94d20dbc015f2511
grg <gdal@pml.ac.uk>
2021-07-08 16:03:14 +0100

    added comment to explain what this is for

--- Scripts/shift_lon.py
+++ Scripts/shift_lon.py
@@ -1,4 +1,5 @@
 #this is needed because the current code generate files with longitudes that are not sorted.
+#if you run again the code, you will need to modify it just before the files are written
 
 #run in parallel using:
 #find /data/datasets/Projects/OLTraj/From_AVISO/OLTraj/Processed/Lagrangian_traj/????/ -name "[1-2]*nc" | xargs -n1 -P1 sh -c 'sleep 2; echo $0' | xargs -n1 -P4 echo python3 shift_lon.py $1
